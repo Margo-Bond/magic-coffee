7af6fa102b8d15bb16b5c012da620862807f140f
Margo-Bond <ritabond1991@gmail.com>
2024-06-21 18:55:29 +0200

    addedfunction

--- ui/components/authorization/authorization.js
+++ ui/components/authorization/authorization.js
@@ -1,3 +1,4 @@
 import { createHeader } from "../header/header.js";
 import { arrowIcon } from "../../../assets/images/forward.svg";
 import { createForwardButton } from "../forward-button/forward-button.js";
+console.log();
