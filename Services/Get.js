a2e31ed3a35a8c27cb67760a95d7ebb14baaa577
Margo-Bond <ritabond1991@gmail.com>
2024-06-23 15:52:10 +0200

    created function getCafes in Get.js

--- Services/Get.js
+++ Services/Get.js
@@ -1,13 +1,15 @@
-export default class Users {
-  static async getUsers() {
+export default class Cafes {
+  static async getCafes() {
     try {
+      console.log("Fetching cafes...");
       const data = await fetch(
-        "https://magic-coffee-878ad-default-rtdb.firebaseio.com/"
+        "https://magic-coffee-878ad-default-rtdb.firebaseio.com/cafes.json"
       );
-      const users = await data.json(users);
-      return users;
+      const cafes = await data.json();
+      console.log("Cafes fetched:", cafes);
+      return cafes;
     } catch (err) {
-      console.error(err);
+      console.error("Error fetching cafes:", err);
       return err;
     }
   }
