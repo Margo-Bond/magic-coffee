0c0834cc75346fcc8eb5a2f06d8c9559a082252c
Margo-Bond <ritabond1991@gmail.com>
2024-06-23 15:20:34 +0200

    fixed header routing, add getUsers function

--- Services/Get.js
+++ Services/Get.js
@@ -1,4 +1,14 @@
 export default class Users {
-  fetch('https://magic-coffee-878ad-default-rtdb.firebaseio.com/');
-  static async getUsers() {}
+  static async getUsers() {
+    try {
+      const data = await fetch(
+        "https://magic-coffee-878ad-default-rtdb.firebaseio.com/"
+      );
+      const users = await data.json(users);
+      return users;
+    } catch (err) {
+      console.error(err);
+      return err;
+    }
+  }
 }
