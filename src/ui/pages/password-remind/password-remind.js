7dc57995daaeaf3e77699861cc68347ecff2760b
Mariia Petukhova <girloflondon@Mariias-MacBook-Pro.local>
2024-06-23 21:38:07 +0300

    Законченная вёрстка регистрации, аторизации и восстановления пароля. Пока без работы над футером и кнопкой forward

--- src/ui/pages/password-remind/password-remind.js
+++ src/ui/pages/password-remind/password-remind.js
@@ -1,6 +1,44 @@
 import { createHeaderBlack } from "@/ui/components/header/header.js";
 import createForwardButton from "@/ui/components/forward-button/forward-button.js";
+import Message from "@/assets/images/user-icons/mail.svg";
 
 export default function renderPasswordRemindPage(main) {
-  main.innerHTML = ``;
+  main.innerHTML = `<div class="password">
+  <button class="password__button"></button>
+        <div class="password__header">
+          <h1 class="password__header-title">Forgot Password?
+</h1>
+          <p class="password__header-text">Enter your email address</p>
+        </div>
+
+        <div class="password__form">
+          
+          <div class="password-form__item">
+            <img class="password-form__item-image" src="${Message}" />
+            <span class="password-form__item-element"></span>
+            <input
+              type="email"
+              class="password-form__item-input input"
+              id="email"
+              placeholder="Email address"
+            />
+          </div>
+
+        </div>  
+  
+        <footer class="password__footer">
+          <button type="submit" class="password-footer__button"></button>
+        </footer>
+        </div>`;
+
+  const back = document.querySelector(".password__button");
+  const next = document.querySelector(".password-footer__button");
+
+  back.addEventListener("click", () => {
+    window.location.href = "/authorization";
+  });
+
+  next.addEventListener("click", () => {
+    window.location.href = "/startup-screen";
+  });
 }
--- src/ui/pages/registration/registration.js
+++ src/ui/pages/registration/registration.js
@@ -1,90 +1,96 @@
 import { createHeaderBlack } from "@/ui/components/header/header.js";
 import createForwardButton from "@/ui/components/forward-button/forward-button.js";
-import profile from "@/assets/images/user-icons/name.svg";
-import smartphone from "@/assets/images/user-icons/phone-number.svg";
-import message from "@/assets/images/user-icons/mail.svg";
-import lock from "@/assets/images/user-icons/password.svg";
-import eye from "@/assets/images/user-icons/visible.svg";
+import Profile from "@/assets/images/user-icons/name.svg";
+import Smartphone from "@/assets/images/user-icons/phone-number.svg";
+import Message from "@/assets/images/user-icons/mail.svg";
+import Lock from "@/assets/images/user-icons/password.svg";
+import Eye from "@/assets/images/user-icons/visible.svg";
 
 export default function renderRegistrationPage(main) {
-  main.innerHTML = `<button class="container__button"></button>
-        <div class="container__header">
-          <h1 class="container__header-title">Sign up</h1>
-          <p class="container__header-text">Create an account here</p>
+  main.innerHTML = `<div class="registration">
+        <button class="registration__button"></button>
+        <div class="registration__header">
+          <h1 class="registration__header-title">Sign up</h1>
+          <p class="registration__header-text">Create an account here</p>
         </div>
 
-        <div class="container__form">
-          <div class="container-form__item">
+        <div class="registration__form">
+          <div class="registration-form__item">
             <img
-              class="container-form__item-image"
-              src="${profile}"
+              class="registration-form__item-image"
+              src="${Profile}"
             />
-            <span class="container-form__item-element"></span>
+            <span class="registration-form__item-element"></span>
             <input
               type="text"
-              class="container-form__item-input input"
+              class="registration-form__item-input input"
               id="username"
               placeholder="Create an account here"
-              required
             />
           </div>
 
-          <div class="container-form__item">
+          <div class="registration-form__item">
             <img
-              class="container-form__item-image"
-              src="${smartphone}"
+              class="registration-form__item-image"
+              src="${Smartphone}"
             />
-            <span class="container-form__item-element"></span>
+            <span class="registration-form__item-element"></span>
             <input
-              type="tel"
-              class="container-form__item-input input"
+              type="number"
+              class="registration-form__item-input input"
               id="phoneNumber"
               placeholder="Mobile Number"
-              required
             />
           </div>
 
-          <div class="container-form__item">
-            <img class="container-form__item-image" src="${message}" />
-            <span class="container-form__item-element"></span>
+          <div class="registration-form__item">
+            <img
+              class="registration-form__item-image"
+              src="${Message}"
+            />
+            <span class="registration-form__item-element"></span>
             <input
               type="email"
-              class="container-form__item-input input"
+              class="registration-form__item-input input"
               id="email"
               placeholder="Email address"
-              required
             />
           </div>
 
-          <div class="container-form__item" id="eye">
-            <img class="container-form__item-image" src="${lock}" />
-            <span class="container-form__item-element"></span>
+          <div class="registration-form__item" id="eye">
+            <img class="registration-form__item-image" src="${Lock}" />
+            <span class="registration-form__item-element"></span>
             <input
               type="password"
-              class="container-form__item-input input"
+              class="registration-form__item-input input"
               id="password"
               placeholder="Password"
-              required
             />
-            <span class="container-form__item-button"
-              ><img class="item-button__img" src="${eye}" />
+            <span class="registration-form__item-button"
+              ><img class="item-button__img" src="${Eye}" />
             </span>
           </div>
 
-          <p class="container__text">
+          <p class="registration__text">
             By signing up you agree with our Terms of Use
           </p>
         </div>
-        <footer class="footer">
-          <p class="footer__text">
-          Already a member? <a class="footer__text-link" id="link">Sign in</a>
-          </p>
-          <button type="submit" class="footer__button" id="button"></button>
-        </footer>`;
+      </main>
+      <footer class="registration__footer">
+        <p class="registration-footer__text">
+          Already a member?
+          <a class="registration-footer__text-link">Sign in</a>
+        </p>
+        <button
+          type="submit"
+          class="registration-footer__button"
+        ></button>
+      </footer>
+    </div>`;
 
-  const back = document.querySelector(".container__button");
-  const signInLink = document.getElementById("link");
-  const next = document.getElementById("button");
+  const back = document.querySelector(".registration__button");
+  const signInLink = document.querySelector(".registration-footer__text-link");
+  const next = document.querySelector(".registration-footer__button");
 
   back.addEventListener("click", () => {
     window.location.href = "/";
