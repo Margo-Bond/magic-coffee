f4b0b54f95713b4332bce2aa302af85fc40efa18
Margo-Bond <ritabond1991@gmail.com>
2024-06-24 22:52:50 +0200

    added Auth.js

--- Services/Auth.js
+++ Services/Auth.js
@@ -1,28 +1,58 @@
-//Временный файл для рагистрации новых пользователей, пока в работе
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "./firebase_config";
+import { getDatabase, ref, runTransaction, set } from "firebase/database";
 
-/*import { auth, database } from "../firebase.js";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { ref, set } from "firebase/database";
 
-async function registerUser(email, password, name, phone) {
-  try {
-    const userCredential = await createUserWithEmailAndPassword(
-      auth,
-      email,
-      password
-    );
-    const userId = userCredential.user.uid;
-    await set(ref(database, "users/" + userId), {
-      name: name,
-      phone: phone,
-      email: email,
-      orders: {},
-    });
-    console.log("User registered:", userId);
-  } catch (error) {
-    console.error("Error creating user:", error);
-    throw error;
-  }
+import { btn } from "./vars";
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+const form = document.querySelector('form');
+const inputName = form.Name;
+const inputEmail = form.Email;
+
+
+function save() {
+  const nameValue = inputName.value;
+  const emailValue = inputEmail.value;
+  const countRef = ref(database, 'userCount');
+
+  runTransaction(countRef, (currentCount) => {
+    if (currentCount === null) {
+      return 1;
+    } else {
+      return Number(currentCount) + 1;
+    }
+  })
+    .then((result) => {
+      if (result.committed) {
+        const newUserCount = result.snapshot.val();
+        const userId = User${newUserCount};
+
+        const userRef = ref(database, 'users/' + userId);
+
+        set(userRef, {
+          name: nameValue,
+          email: emailValue
+        })
+          .then((data) => {
+            console.log('data created')
+          })
+          .catch((error) => {
+            console.log('Error');
+          })
+      } else {
+        console.log('Transaction wasnt committed');
+      }
+    })
+    .catch((error) => {
+      console.log('error');
+    })
 }
 
-export { registerUser };*/
+
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  save();
+});
