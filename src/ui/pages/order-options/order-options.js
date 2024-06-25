50b7d44d77a53c14bc5f9371c6b6c58e973f6689
odnomgnovenie <dadavygora@gmail.com>
2024-06-25 16:38:48 +0300

    created js and css in page #8

--- src/ui/pages/order-options/order-options.js
+++ src/ui/pages/order-options/order-options.js
@@ -11,23 +11,23 @@ import cupMedium from "@/assets/images/coffee-icons/cup-350.svg";
 import cupBig from "@/assets/images/coffee-icons/cup-450.svg";
 
 
-const imageCoffee = "https://img.freepik.com/free-photo/fresh-coffee-steams-wooden-table-close-up-generative-ai_188544-8923.jpg";
+const imageCoffee = "https://imgproxy.sbermarket.ru/imgproxy/width-auto/czM6Ly9jb250ZW50LWltYWdlcy1wcm9kL3Byb2R1Y3RzLzMyNDQxNTEyL29yaWdpbmFsLzEvMjAyNC0wMy0xOFQxMSUzQTA1JTNBNDQuMjkwODczJTJCMDAlM0EwMC8zMjQ0MTUxMl8xLmpwZw==.jpg";
 
 export default function renderOrderOptionPage(main) {
   main.innerHTML = `
         <div class="container-order">
           <div class="container-order__main">
-            <a class="container-order__btn">
+            <a class="back">
               <img src='${arrowBack}'  alt='Back btn' />
             </a>
             <p class="сontainer-order__title">Order</p>
-            <a class="container-order__btn">
-              <img src='${cartBuy}' alt='Cart btn' />
+            <a class="card">
+              <img src='${cartBuy}' alt='Card btn' />
             </a>
           </div>
 
-      <div class="container-order__item">
-        <div class="container-order__item_wrap">
+      <div class="container-order__coffee">
+        <div class="container-order__wrap">
           <img
             class="container-order__image"
             src="${imageCoffee}"
@@ -50,9 +50,9 @@ export default function renderOrderOptionPage(main) {
         <div class="container-order__item-text">
           <p class="container-order__text">Ristretto</p>
         </div>
-        <div class="container-order__item-select">
-          <span class="container-order__item-select_num">One</span>
-          <span class="container-order__item-select_num">Two</span>
+        <div class="container-order__item-strength">
+          <button id="strength1" class="container-order__btn">One</button>
+          <button id="strength2"class="container-order__btn">Two</button>
         </div>
       </div>
 
@@ -60,12 +60,14 @@ export default function renderOrderOptionPage(main) {
         <div class="container-order__item-text">
           <p class="container-order__text">Onsite / Takeaway</p>
         </div>
-        <div class="container-order__item-select">
+        <div class="container-order__item-temperature">
           <img
-            class="container-order__item-image"
+          id="hot"
+            class="container-order__temperature"
             src="${hotBeverage}"/>
           <img
-            class="container-order__item-image"
+           id="cold"
+            class="container-order__temperature"
             src="${coldBeverage}"/>
         </div>
       </div>
@@ -74,18 +76,19 @@ export default function renderOrderOptionPage(main) {
         <div class="container-order__item-text">
           <p class="container-order__text"> Volume, ml </p>
         </div>
-        <div class="container-order__item-select">
+        <div class="container-order__item-cup-size">
           <img
-            class="container-order__item-image"
+            class="container-order__cup-size container-order__cup-size_small"
             src="${cupSmall}"/>
           <img
-            class="container-order__item-image"
+            class="container-order__cup-size container-order__cup-size_medium"
             src="${cupMedium}"/>
           <img
-            class="container-order__item-image"
+            class="container-order__cup-size ccontainer-order__cup-size-big"
             src="${cupBig}"/>  
         </div>
       </div>
+
       <div class="container-order__item">
         <div class="container-order__item-text">
           <p class="container-order__text">Prepare by a certain time today?</p>
@@ -115,19 +118,29 @@ export default function renderOrderOptionPage(main) {
       </div>
     </footer>`;
 
-  const back = document.querySelector(".container-order__button");
-  const signInLink = document.getElementById("link");
-  const next = document.getElementById("button");
+  const back = document.querySelector(".back");
+  const card = document.querySelector(".card");
 
   back.addEventListener("click", () => {
-    window.location.href = "/";
+    window.location.href = "/menu";
+  })
+
+  card.addEventListener("click", () => {
+    window.location.href = "/my-oders";
   });
 
-  signInLink.addEventListener("click", () => {
-    window.location.href = "/";
-  });
+  document.addEventListener('DOMContentLoaded', () => {
+    const one = document.getElementById("strength1");
+    const two = document.getElementById("strength2");
 
-  next.addEventListener("click", () => {
-    window.location.href = "/";
+    function highlightElement(strength) {
+      one.classList.remove('highlight');
+      two.classList.remove('highlight');
+      strength.classList.add('highlight');
+    }
+
+    one.addEventListener('click', () => highlightElement(one));
+    two.addEventListener('click', () => highlightElement(two));
   });
 }
+
