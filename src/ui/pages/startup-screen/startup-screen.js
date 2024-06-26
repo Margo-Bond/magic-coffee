36b725704d01e822d30b45dbe320d2f95a7d3fa1
odnomgnovenie <dadavygora@gmail.com>
2024-06-26 11:23:09 +0300

    create page #5

--- src/ui/pages/startup-screen/startup-screen.js
+++ src/ui/pages/startup-screen/startup-screen.js
@@ -1,5 +1,11 @@
-import { createHeaderWhite } from "@/ui/components/header/header.js";
+import Logo from "@/assets/images/logo.svg";
 
-export default function renderStartupScreenPage(main) {
-  main.innerHTML = ``;
+export default function renderStartupScreenPage(body) {
+  body.innerHTML = `    
+    <div class="startup__logo">
+      <img src='${Logo}' alt='Logo' />
+      <h1 class="startup__title">Magic coffee</h1>`;
+  setTimeout(function () {
+    window.location.href = '/menu';
+  }, 1 * 1000);
 }
