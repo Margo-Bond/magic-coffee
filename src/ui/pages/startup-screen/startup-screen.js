51a8235d3d6a06ff1696409907e7b136acbb826c
odnomgnovenie <dadavygora@gmail.com>
2024-06-26 17:33:35 +0300

    Last changes

--- src/ui/pages/startup-screen/startup-screen.js
+++ src/ui/pages/startup-screen/startup-screen.js
@@ -6,7 +6,10 @@ export default function renderStartupScreenPage(main) {
       <img src='${Logo}' alt='Logo' />
       <h1 class="startup__title">Magic coffee</h1>`;
 
+  const div = document.querySelector('.container');
+  div.classList.add('startup-background');
+
   setTimeout(function () {
     window.location.href = '/menu';
-  }, 2 * 1000);
+  }, 2000);
 }
