669283cb1fa6e1b708084bbb307aecfde27254db
Margo-Bond <ritabond1991@gmail.com>
2024-06-26 22:56:20 +0200

    created white header on startup pafe

--- src/ui/components/header/header.js
+++ src/ui/components/header/header.js
@@ -1,30 +1,42 @@
-import Time from "@/assets/images/header/header-time.svg";
-import Connection from "@/assets/images/header/header-connection.svg";
-import Wifi from "@/assets/images/header/header-wifi.svg";
-import Battery from "@/assets/images/header/header-battery.svg";
+import TimeSvg from "@/assets/images/header/header-time.svg";
+import ConnectionSvg from "@/assets/images/header/header-connection.svg";
+import WifiSvg from "@/assets/images/header/header-wifi.svg";
+import BatterySvg from "@/assets/images/header/header-battery.svg";
 
 export function createHeaderBlack(header) {
   header.innerHTML = `
-      <div>
-        <img src='${Time}' alt='Current time' />
-      </div>
-      <div>
-        <img src='${Connection}' alt='Current connection' />
-        <img src='${Wifi}' alt='Wifi statment' />
-        <img src='${Battery}' alt='Battery statment' />
-      </div>
-    `;
+    <div class="header__wrapper">
+      <div class="header-icon">${TimeSvg}</div>
+    </div>
+    <div class="header__wrapper">
+      <div class="header-icon">${ConnectionSvg}</div>
+      <div class="header-icon">${WifiSvg}</div>
+      <div class="header-icon">${BatterySvg}</div>
+    </div>
+  `;
 }
 
 export function createHeaderWhite(header) {
   header.innerHTML = `
-    <div>
-        <img src='${Time}' alt='Current time' />
-      </div>
-      <div>
-        <img src='${Connection}' alt='Current connection' />
-        <img src='${Wifi}' alt='Wifi statment' />
-        <img src='${Battery}' alt='Battery statment' />
-      </div>
-    `;
+    <div class="header__wrapper">
+      <div class="header-icon header-white__icon">${TimeSvg}</div>
+    </div>
+    <div class="header__wrapper">
+      <div class="header-icon header-white__icon">${ConnectionSvg}</div>
+      <div class="header-icon header-white__icon">${WifiSvg}</div>
+      <div class="header-icon header-white__icon">${BatterySvg}</div>
+    </div>
+  `;
+
+  changeSvgFill(header, "white");
+}
+
+function changeSvgFill(header, color) {
+  const paths = header.querySelectorAll(
+    ".header-white__icon svg path, .header-white__icon svg rect"
+  );
+  paths.forEach((path) => {
+    path.setAttribute("fill", color);
+    path.setAttribute("stroke", color);
+  });
 }
