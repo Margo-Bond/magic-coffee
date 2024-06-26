78038cfd3cee7e99edc5aeb932f09c40c39bfc13
odnomgnovenie <dadavygora@gmail.com>
2024-06-26 14:43:57 +0300

    changed js file

--- src/ui/pages/startup-screen/startup-screen.js
+++ src/ui/pages/startup-screen/startup-screen.js
@@ -1,13 +1,11 @@
 
 import Logo from "@/assets/images/logo.svg";
 
-export default function renderStartupScreenPage(body) {
-  body.innerHTML = `    
+export default function renderStartupScreenPage(main) {
+  main.innerHTML = `    
     <div class="startup__logo">
       <img src='${Logo}' alt='Logo' />
       <h1 class="startup__title">Magic coffee</h1>`;
-
-  document.getElementById('white').setAttribute('fill', 'white');
 
   setTimeout(function () {
     window.location.href = '/menu';
