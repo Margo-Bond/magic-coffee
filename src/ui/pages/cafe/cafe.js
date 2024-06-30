9073158701604a2d1265b007b5a1906b13e0a36c
Anna Zhukovskaya <avzhukovskaya@gmail.com>
2024-06-30 19:17:43 +0300

    svg doesn't work

--- src/ui/pages/cafe/cafe.js
+++ src/ui/pages/cafe/cafe.js
@@ -1,5 +1,65 @@
-import { createHeaderBlack } from "@/ui/components/header/header.js";
+import Cafe from "../../../assets/images/cafe.svg";
+import Forward from "../../../assets/images/geometric-icons/forward.svg";
 
 export default function renderCafePage(main) {
-  main.innerHTML = ``;
+
+  main.innerHTML = `
+    <div class="cafe">
+
+      <div class="cafe__button">
+        <button class="cafe__button-icon"></button>
+      </div>
+
+      <div class="cafe__box">
+        <div class="cafe-box__element-title">
+          <p class="cafe-box__element-text">Select Magic Coffee store</p>
+        </div>
+
+        <div class="cafe-box__element-wrap">
+
+          <div class="cafe-box__element">
+
+            <div class="cafe-box__button">
+              <div class="cafe-box__button-icon">
+                <img src='${Cafe}' alt='Cafe' />
+              </div>
+    
+              <div class="cafe-box__button-title">Bradford BD4 7SJ</div>
+    
+              <div class="cafe-box__button-arrow">
+                <img src='${Forward}' alt='Arrow' />
+              </div>
+            </div>
+
+            <div class="cafe-box__button">
+              <div class="cafe-box__button-icon">
+                <img src='${Cafe}' alt='Cafe' />
+              </div>
+    
+              <div class="cafe-box__button-title">Bradford BD4 7SJ</div>
+    
+              <div class="cafe-box__button-arrow">
+                <img src='${Forward}' alt='Arrow' />
+              </div>
+            </div>
+
+            <div class="cafe-box__button">
+              <div class="cafe-box__button-icon">
+                <img src='${Cafe}' alt='Cafe' />
+              </div>
+    
+              <div class="cafe-box__button-title">Bradford BD4 7SJ</div>
+    
+              <div class="cafe-box__button-arrow">
+                <img src='${Forward}' alt='Arrow' />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
 }
+
+renderCafePage(main);
+
