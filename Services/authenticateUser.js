9ec96c7d6808db6ab3e482804da420e43b4d6157
girloflondon <girloflondon@gmail.com>
2024-07-01 19:06:57 +0300

    test 2

--- Services/authenticateUser.js
+++ Services/authenticateUser.js
@@ -1,39 +1,3 @@
-//Функция на авторизацию
-//для страниц: #2, 4
-/*
-import { auth, signInWithEmailAndPassword } from "../main.js";
-
-export default async function authenticateUser(email, password = null) {
-  try {
-    const userCredential = await signInWithEmailAndPassword(
-      auth,
-      email,
-      password
-    );
-    const user = userCredential.user;
-
-    // Сохраняем данные пользователя в Local Storage
-    localStorage.setItem(
-      "user",
-      JSON.stringify({
-        uid: user.uid,
-        name: user.name,
-        phoneNumber: user.userNumber,
-        email: user.email,
-        password: user.password,
-        cart: user.cart,
-        currentOrders: user.currentOrders,
-        orderHistory: user.orderHistory,
-        // другие данные, если нужно
-      })
-    );
-    return user;
-  } catch (error) {
-    throw new Error("Authentication failed: " + error.message);
-  }
-}
-*/
-
 import { auth, signInWithEmailAndPassword } from "../main.js";
 import { ref, get, child } from "firebase/database";
 import { database } from "../main.js";
@@ -48,7 +12,7 @@ export default async function authenticateUser(email, password) {
     const user = userCredential.user;
 
     // Получение дополнительных данных пользователя из базы данных
-    const userRef = ref(database, users / `${user.uid}`);
+    const userRef = ref(database, `users/${user.uid}`);
     const snapshot = await get(userRef);
 
     if (!snapshot.exists()) {
