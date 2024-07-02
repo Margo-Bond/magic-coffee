7df47e1b940c0c3f8feac3b4a57824dd36637c72
odnomgnovenie <dadavygora@gmail.com>
2024-07-02 23:24:12 +0300

    added new css

--- src/ui/pages/order-options/order-options.js
+++ src/ui/pages/order-options/order-options.js
@@ -2,11 +2,11 @@
 import ArrowBack from "@/assets/images/geometric-icons/back.svg";
 import CartBuy from "@/assets/images/cart.svg";
 import CoffeeCard from "@/assets/images/coffee-cup.svg";
-import coldBeverage from "@/assets/images/coffee-icons/cold-beverage.svg";
-import hotBeverage from "@/assets/images/coffee-icons/hot-beverage.svg";
-import cupSmall from "@/assets/images/coffee-icons/cup-250.svg";
-import cupMedium from "@/assets/images/coffee-icons/cup-350.svg";
-import cupBig from "@/assets/images/coffee-icons/cup-450.svg";
+import ColdBeverage from "@/assets/images/coffee-icons/cold-beverage.svg";
+import HotBeverage from "@/assets/images/coffee-icons/hot-beverage.svg";
+import CupSmall from "@/assets/images/coffee-icons/cup-250.svg";
+import CupMedium from "@/assets/images/coffee-icons/cup-350.svg";
+import CupLardge from "@/assets/images/coffee-icons/cup-450.svg";
 
 export default function renderOrderOptionPage(main) {
   main.innerHTML = `
@@ -25,10 +25,10 @@ export default function renderOrderOptionPage(main) {
         <div class="order-option__item-text">
           <p class="order-option__text">Cuppucino</p>
         </div>
-        <div class="order-optionr__item-quantity">
-          <div class="order-option__quantity order-option__quantity_minus">-</div>
-          <div class="order-option__quantity order-option__quantity_number">0</div>
-          <div class="order-option__quantity order-option__quantity_plus">+</div>
+        <div class="order-optionr__item-quantity" id="counter"> 
+          <input type="button"  class="order-option__quantity id="buttonCountMinus" value="-">
+          <div id="buttonCountNumber">1</div>
+          <input type="button" class="order-option__quantity id="buttonCountPlus" value="+">
         </div>
       </div>
 
@@ -38,7 +38,7 @@ export default function renderOrderOptionPage(main) {
         </div>
         <div class="order-option__item-strength">
           <button id="strength1" class="order-option__strength">One</button>
-          <button id="strength2"class="order-optionм__strength">Two</button>
+          <button id="strength2"class="order-option__strength">Two</button>
         </div>
       </div>
 
@@ -47,14 +47,8 @@ export default function renderOrderOptionPage(main) {
           <p class="order-option__text">Onsite / Takeaway</p>
         </div>
         <div class="order-optionr__item-temperature">
-          <img
-          id="hot"
-            class="order-option__temperature"
-            src="${hotBeverage}"/>
-          <img
-           id="cold"
-            class="order-option__temperature"
-            src="${coldBeverage}"/>
+          <div id="hot" class="order-option__temperature" >${HotBeverage}</div>
+          <div id="cold" class="order-option__temperature" >${ColdBeverage}</div>
         </div>
       </div>
 
@@ -63,47 +57,43 @@ export default function renderOrderOptionPage(main) {
           <p class="order-option__text"> Volume, ml </p>
         </div>
         <div class="order-option__item-cup">
-           <div order-option__cup-size" 
-           <div order-option__cup-size" 
-           <div order-option__cup-size" 
-         </div>   
-         
-        /* <div class="order-option__item-cup">
-          <img id="cold"
-            class="order-option__cup-size "
-            src="${cupSmall}"/>
-          <img
-            class="container-order__cup-size container-order__cup-size_medium"
-            src="${cupMedium}"/>
-          <img
-            class="container-order__cup-size ccontainer-order__cup-size-big"
-            src="${cupBig}"/>  
-        </div> */
+          <div class="order-option__cup">
+            <div id="small" class="order-option__cup-size" >${CupSmall}</div>
+            <div id="medium" class="order-option__cup-size" >${CupMedium}</div>
+            <div id="lardge" class="order-option__cup-size" >${CupLardge}</div>
+          </div>
+          <div class="order-option__cup">
+            <div class="order-option__size" >250</div>
+            <div class="order-option__size" >350</div>
+            <div class="order-option__size" >450</div>
+          </div>
+        </div>  
       </div>
 
-      <div class="container-order__item">
-        <div class="container-order__item-text">
-          <p class="container-order__text">Prepare by a certain time today?</p>
+      <div class="order-option__item">
+        <div class="order-option__item-text">
+          <p class="order-option__text">Prepare by a certain time today?</p>
         </div>
-        <div class="container-order__item_switch">
+        <div class="order-option__item_switch">
           <input type="checkbox" class="switch__input" />
           <span class-"switch__slider"></span>
         </div>
       </div>
-      <div class="container-order__item">
-        <div class="container-order__item-empty"></div>
-        <div class="container-order__item-watch">
-          <span class="container-order__item-watch_hour">00</span>
-          <span class="container-order__item-watch_point">:</span>
-          <span class="container-order__item-watch_minutes">00</span>
+
+      <div class="order-option__item">
+        <div class="order-option__item-empty"></div>
+        <div class="order-option__item-watch">
+          <span id="hour" class="order-option__watch">00</span>
+          <span id="dots" class="order-option__watch>:</span>
+          <span id="minutes" class="order-option__watch">00</span>
         </div>
       </div>
     </div>
     
     <footer class="footer">
       <div class="footer__content"
-        <p class="footer__text"> Total Amount </p>
-        <a class="footer__sum" id="link">BYN 3.00</a>
+        <div class="footer__text"> Total Amount </div>
+        <div id="calculation">150</div> 
       </div>
       <div class="footer__button">
         <button type="submit" id="button">Next</button>
@@ -124,34 +114,19 @@ export default function renderOrderOptionPage(main) {
   const coffeeCard = main.querySelector(".coffeeCard");
   coffeeCard.insertAdjacentHTML("afterbegin", CoffeeCard);
 
-  const minusBtn = main.querySelector(".order-option__quantity_minus");
-  const numberBtn = main.querySelector(".order-option__quantity_number");
-  const plusBtn = main.querySelector(".order-option__quantity_plus");
+  document.addEventListener('DOMContentLoaded', () => {
+    const one = document.getElementById("strength1");
+    const two = document.getElementById("strength2");
 
-  function setActiveIcon(activeBtn) {
-    const allIcons = footer.querySelectorAll(".footer__icon svg path");
-    allIcons.forEach((icon) => {
-      icon.style.fill = "var(--svg-color-muted)";
-    });
+    function highlightElement(strength) {
+      one.classList.remove('highlight');
+      two.classList.remove('highlight');
+      strength.classList.add('highlight');
+    }
 
-
-
-
-
-
-    document.addEventListener('DOMContentLoaded', () => {
-      const one = document.getElementById("strength1");
-      const two = document.getElementById("strength2");
-
-      function highlightElement(strength) {
-        one.classList.remove('highlight');
-        two.classList.remove('highlight');
-        strength.classList.add('highlight');
-      }
-
-      one.addEventListener('click', () => highlightElement(one));
-      two.addEventListener('click', () => highlightElement(two));
-    });
-  }
+    one.addEventListener('click', () => highlightElement(one));
+    two.addEventListener('click', () => highlightElement(two));
+  });
 }
+
 
