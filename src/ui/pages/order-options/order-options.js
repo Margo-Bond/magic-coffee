a166f5687ccc6593cd4ad868ac8e0aef65e95846
odnomgnovenie <dadavygora@gmail.com>
2024-07-06 16:26:54 +0300

    added new commits

--- src/ui/pages/order-options/order-options.js
+++ src/ui/pages/order-options/order-options.js
@@ -2,21 +2,20 @@
 import ArrowBack from "@/assets/images/geometric-icons/back.svg";
 import CartBuy from "@/assets/images/cart.svg";
 import CoffeeCard from "@/assets/images/coffee-cup.svg";
-import ColdBeverage from "@/assets/images/coffee-icons/cold-beverage.svg";
-import HotBeverage from "@/assets/images/coffee-icons/hot-beverage.svg";
+import Takeaway from "@/assets/images/coffee-icons/cold-beverage.svg";
+import Onsite from "@/assets/images/coffee-icons/hot-beverage.svg";
 import CupSmall from "@/assets/images/coffee-icons/cup-250.svg";
 import CupMedium from "@/assets/images/coffee-icons/cup-350.svg";
 import CupLarge from "@/assets/images/coffee-icons/cup-450.svg";
-import { auth, database, onAuthStateChanged } from "../../../../main.js";
 
 export default function renderOrderOptionPage(main) {
   main.innerHTML = `
-        <div class="order-option">
-          <div class="order-option__main">
-            <div class="order-option__arrowBack">${ArrowBack}</div>
-            <p class="order-option__title">Order</p>
-            <div class="order-option__cartBuy">${CartBuy}</div>
-          </div>
+    <div class="order-option">
+      <div class="order-option__main">
+        <div class="order-option__arrowBack">${ArrowBack}</div>
+        <p class="order-option__title">Order</p>
+        <div class="order-option__cartBuy">${CartBuy}</div>
+      </div>
 
       <div class="order-option__coffee-img">
         <div class="coffeeCard">${CoffeeCard}</div>
@@ -26,15 +25,13 @@ export default function renderOrderOptionPage(main) {
         <div class="order-option__item-text">
           <p class="order-option__text">Cuppucino</p>
         </div>
-
-
         <div class="order-optionr__item-quantity" id="counter"> 
           <input type="button"  class="order-option__quantity" id="buttonCountMinus" value="-">
           <div id="buttonCountNumber">1</div>
-          <input type="button" class="order-option__quantity id="buttonCountPlus" value="+">
+          <input type="button" class="order-option__quantity" id="buttonCountPlus" value="+">
         </div>
-
       </div>
+
       <div class="order-option__item">
         <div class="order-option__item-text">
           <p class="order-option__text">Ristretto</p>
@@ -49,9 +46,9 @@ export default function renderOrderOptionPage(main) {
         <div class="order-option__item-text">
           <p class="order-option__text">Onsite / Takeaway</p>
         </div>
-        <div class="order-optionr__item-temperature">
-          <div id="hot" class="order-option__temperature" >${HotBeverage}</div>
-          <div id="cold" class="order-option__temperature" >${ColdBeverage}</div>
+        <div class="order-option__where">
+          <div class="order-option__svg order-option__svg-where_onsite" >${Onsite}</div>
+          <div class="order-option__svg order-option__svg-where_takeaway" >${Takeaway}</div>
         </div>
       </div>
 
@@ -61,16 +58,16 @@ export default function renderOrderOptionPage(main) {
         </div>
         <div class="order-option__item-cup">
           <div class="order-option__cup">
-            <div id="small" class="order-option__cup-size" >${CupSmall}</div>
-            <div class="order-option__size" >250</div>
+            <div class="order-option__svg order-option__svg-cup_small" >${CupSmall}</div>
+            <div class="order-option__text-size" >250</div>
           </div>
           <div class="order-option__cup">
-            <div id="medium" class="order-option__cup-size" >${CupMedium}</div>
-            <div class="order-option__size" >350</div>
+            <div class="order-option__svg order-option__svg-cup_medium" >${CupMedium}</div>
+            <div class="order-option__text-size" >350</div>
           </div>
           <div class="order-option__cup">
-            <div id="large" class="order-option__cup-size" >${CupLarge}</div>
-            <div class="order-option__size" >450</div>
+            <div class="order-option__svg order-option__svg-cup_large" >${CupLarge}</div>
+            <div class="order-option__text-size" >450</div>
           </div>
         </div>  
       </div>
@@ -80,29 +77,22 @@ export default function renderOrderOptionPage(main) {
           <p class="order-option__text">Prepare by a certain time today?</p>
         </div>
         <div class="order-option__item_switch">
-          <input type="checkbox" id="togBtn" />
-          <span class="slider round"></span>
+          <label class="switch">
+            <input type="checkbox" id="togBtn" />
+            <span class="slider round"></span>
+          </label>
         </div>
-      </div>
-
-      <div class="order-option__item">
-        <div class="order-option__item-empty"></div>
         <div class="order-option__item-watch">
-          <span id="hour" class="order-option__watch">00</span>
-          <span id="dots" class="order-option__watch>:</span>
-          <span id="minutes" class="order-option__watch">00</span>
+          <input type="time" id="time" class="order-option__watch" value="00:00" />
         </div>
-      </div>
     </div>
-    
-    <footer class="footer">
-      <div class="footer__content"
-        <div class="footer__text"> Total Amount </div>
-        <div id="calculation"></div> 
+
+    <footer class="order-option-footer">
+      <div class="order-option-footer__count-container"
+        <p class="order-option-footer__count-text"> Total Amount </p>
+        <p class="order-option-footer__count-currency">BYN <span id="multipliedValue" class="order-option-footer__count">1</span></p>
       </div>
-      <div class="footer__button">
-        <button type="submit" id="button">Next</button>
-      </div>
+      <button class="order-option-footer__button">Next</button>
     </footer>`;
 
   const back = document.querySelector(".order-option__arrowBack");
@@ -113,50 +103,43 @@ export default function renderOrderOptionPage(main) {
   })
 
   cart.addEventListener("click", () => {
-    window.location.href = "/my-oders";
+    window.location.href = "/current-order";
   });
 
-  /////
+  ///
 
   document.addEventListener('DOMContentLoaded', function () {
-    // Находим элементы на странице
-    const countMinus = document.getElementById('buttonCountMinus');
-    const countPlus = document.getElementById('buttonCountPlus');
-    const countNumber = document.getElementById('buttonCountNumber');
+    const counter = document.getElementById('buttonCountNumber');
+    const buttonPlus = document.getElementById('buttonCountPlus');
+    const buttonMinus = document.getElementById('buttonCountMinus');
+    let count = 1;
+    const minCount = 1;
+    const multiplier = 3;
 
-    // Инициализируем счетчик
-    const counter = parseInt(countNumber.textContent, 10);
-
-    // Функция для обновления отображаемого числа
-    function updateDisplay() {
-      countNumber.textContent = counter;
+    function updateCount(newCount) {
+      count = newCount;
+      counter.textContent = count;
+      document.getElementById('multipliedValue').textContent = count * multiplier;
     }
 
-    // Функция для уменьшения счетчика
-    function decreaseCount() {
-      if (counter > 0) { // Предотвращаем отрицательное значение
-        counter--;
-        updateDisplay();
+    buttonPlus.addEventListener('click', function () {
+      if (count) {
+        updateCount(count + 1);
       }
-    }
+    });
 
-    // Функция для увеличения счетчика
-    function increaseCount() {
-      counter++;
-      updateDisplay();
-    }
+    buttonMinus.addEventListener('click', function () {
+      if (count > minCount) {
+        updateCount(count - 1);
+      }
+    });
 
-    // Добавляем обработчики событий для кнопок
-    countMinus.addEventListener('click', decreaseCount);
-    countPlus.addEventListener('click', increaseCount);
+    updateCount(count);
   });
 
+  ///
 
-
-
-  //////
   const buttons = document.querySelectorAll('.order-option__strength');
-
   function resetButtons() {
     buttons.forEach(button => {
       button.classList.remove('active');
@@ -170,78 +153,116 @@ export default function renderOrderOptionPage(main) {
     });
   });
 
-  /*   one.addEventListener('click', () => highlightElement(one));
-    two.addEventListener('click', () => highlightElement(two));
-  
-    document.addEventListener('DOMContentLoaded', () => {
-      const one = document.getElementById("strength1");
-      const two = document.getElementById("strength2");
-  
-      function highlightElement(strength) {
-        one.classList.remove('highlight');
-        two.classList.remove('highlight');
-        strength.classList.add('highlight');
-      }
-    }); */
+  ///
 
+  const onsite = document.querySelector(".order-option__svg-where_onsite");
+  const takeaway = document.querySelector(".order-option__svg-where_takeaway");
+  const cupSizeSmall = document.querySelector(".order-option__svg-cup_small");
+  const cupSizeMedium = document.querySelector(".order-option__svg-cup_medium");
+  const cupSizeLarge = document.querySelector(".order-option__svg-cup_large");
 
-  ////////
-  const cupSizeSmall = document.getElementById("small");
-  const cupSizeMedium = document.getElementById("medium");
-  const cupSizeLarge = document.getElementById("large");
-  const hotBeverage = document.getElementById("hot");
-  const coldBeverage = document.getElementById("cold");
+  function setFillColor(element, color) {
+    element.querySelectorAll("svg path, svg rect").forEach((svg) => {
+      svg.setAttribute("fill", color);
+      svg.setAttribute("stroke", color);
+      svg.setAttribute("stroke-width", "0.1");
+    });
+  }
 
-  const elements = [
-    { element: cupSizeSmall, related: [cupSizeMedium, cupSizeLarge] },
-    { element: cupSizeMedium, related: [cupSizeSmall, cupSizeLarge] },
-    { element: cupSizeLarge, related: [cupSizeSmall, cupSizeMedium] },
-  ];
-  const elementsBev = [
-    { element: hotBeverage, related: [coldBeverage] },
-    { element: coldBeverage, related: [hotBeverage] },
-  ];
+  function checkBlack(element, type, category) {
+    const isBlack = Array.from(
+      element.querySelectorAll("svg path, svg rect")
+    ).some((svg) => svg.getAttribute("fill") === "black");
 
-  elements.forEach((item) => {
-    item.element.addEventListener("click", () => {
-      if (isElementFilled(item.element)) {
-        resetFillColor(item.element);
+    if (isBlack) {
+      selectCategory(type, category, "#D8D8D8");
+      localStorage.removeItem(category);
+    } else {
+      selectCategory(type, category, "black");
+    }
+  }
+
+  function selectCategory(type, category, color) {
+    const options = {
+      where: ["onsite", "takeaway"],
+      cup: ["small", "medium", "large"],
+    };
+    options[category].forEach((option) => {
+      const elements = document.querySelectorAll(
+        `.order-option__svg-${category}_${option}`
+      );
+
+      elements.forEach((element) => {
+        if (option === type) {
+          setFillColor(element, color);
+          if (color === "black") {
+            localStorage.setItem(category, type);
+          }
+        } else {
+          setFillColor(element, "#D8D8D8");
+        }
+      });
+    });
+  }
+  onsite.addEventListener("click", () =>
+    checkBlack(onsite, "onsite", "where")
+  );
+  takeaway.addEventListener("click", () =>
+    checkBlack(takeaway, "takeaway", "where")
+  );
+  cupSizeSmall.addEventListener("click", () =>
+    checkBlack(cupSizeSmall, "small", "cup")
+  );
+
+  cupSizeMedium.addEventListener("click", () =>
+    checkBlack(cupSizeMedium, "medium", "cup")
+  );
+  cupSizeLarge.addEventListener("click", () =>
+    checkBlack(cupSizeLarge, "large", "cup")
+  );
+
+  ///
+  document.addEventListener('DOMContentLoaded', function () {
+    const toggle = document.getElementById('togBtn');
+    const watch = document.querySelector('.order-option__item-watch');
+
+    toggle.addEventListener('change', function () {
+      if (this.checked) {
+        watch.style.display = 'flex';
       } else {
-        resetAllColors();
-        changeFillColor(item.element);
+        watch.style.display = 'none';
       }
     });
   });
 
-  function isElementFilled(element) {
-    const svgElements = element.querySelectorAll("svg path, svg rect");
-    return Array.from(svgElements).some(
-      (svgElement) => svgElement.getAttribute("fill") === "black"
-    );
-  }
+  /*   document.addEventListener('DOMContentLoaded', function () {
+      const toggleButton = document.getElementById('togBtn');
+  
+      toggleButton.addEventListener('change', function () {
+        if (this.checked) {
+     time.addEventListener("input", (event) => {
+        const value = event.target.value;
+        localStorage.setItem("coffee_type", value);
+      });
+        } else {
+          console.log("Toggled off");
+          // Добавьте сюда код, который должен выполняться при выключении тоглера
+        }
+      });
+    }); 
+    
+    
+    
+      onAuthStateChanged(auth, (user) => {
+    if (user) {
+      slider.addEventListener("span", (event) => {
+        const value = event.target.value;
+        localStorage.setItem("coffee_type", value);
+      });
+    } else {
+      alert("You should sign in.");
+      console.log("No user is signed in.");
+    }
+  });*/
 
-  function changeFillColor(element) {
-    const svgElements = element.querySelectorAll("svg path, svg rect");
-    svgElements.forEach((svgElement) => {
-      svgElement.setAttribute("fill", "black");
-      svgElement.setAttribute("stroke", "black");
-      svgElement.setAttribute('stroke-width', '0.1');
-    });
-  }
-
-  function resetFillColor(element) {
-    const svgElements = element.querySelectorAll("svg path, svg rect");
-    svgElements.forEach((svgElement) => {
-      svgElement.setAttribute("fill", "#D8D8D8");
-      svgElement.setAttribute("stroke", "#D8D8D8");
-      svgElement.setAttribute('stroke-width', '0.1');
-    });
-  }
-
-  function resetAllColors() {
-    elements.forEach((item) => {
-      resetFillColor(item.element);
-    });
-  }
 }
-
