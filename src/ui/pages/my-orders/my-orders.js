e985d68fca3d3cec91b70b559be99d6ba0d8b0b9
girloflondon <girloflondon@gmail.com>
2024-07-13 01:52:37 +0300

    пытаюсь разобраться с Дашиным кодом

--- src/ui/pages/my-orders/my-orders.js
+++ src/ui/pages/my-orders/my-orders.js
@@ -1,74 +1,59 @@
-import { createHeaderBlack } from "@/ui/components/header/header.js";
+//Дашин код
+import CoffeeCupImg from "src/assets/images/coffee-cups.svg";
+import cafeAddressImg from "src/assets/images/Location-icon.svg";
 
-export default function renderMyOrdersPage(main) {
-  main.innerHTML = `
-  <main class="myOrders">
+let order = JSON.parse(localStorage.getItem("order")) || {};
+let keys = Object.keys(order);
+let lastKey = keys[keys.length - 1];
 
-          <h1 class="myOrders__header">My orders</h1>
+/* function formatDateTime(dateTime) { 
+  const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' }; 
+  return new Date(dateTime).toLocaleDateString('en-GB', options); 
+} 
+const formattedDateTime = formatDateTime(new Date()); */
 
-        <div class="myOrders__nav">
-          <div class="myOrders-nav__item">
-            <button type="button" class="myOrders-nav__item-button border" id="onGoing">On going</button>
-            <span class="myOrders-nav__item-element"></span>
-          </div> 
-          <div class="myOrders-nav__item">
-            <button type="button" class="myOrders-nav__item-button" id="history">History</button>
-            <span class="myOrders-nav__item-element invisible"></span>
-          </div>
-        </div>
-          
-        <div class="myOrders__table1">
+export default async function renderMyOrdersPage(main) {
+  main.innerHTML = ` 
+    <div class="myOrders"> 
+    <div class="myOrders__main"> 
+      <p class="myOrders__title">My Orders</p> 
+    </div> 
+ 
+    <div class="myOrders__nav"> 
+      <div class="myOrders-nav__item"> 
+        <button type="button" class="myOrders-nav__item-button border"  id="onGoing">On going</button> 
+        <span class="myOrders-nav__item-element"></span> 
+      </div>  
+      <div class="myOrders-nav__item"> 
+        <button type="button" class="myOrders-nav__item-button" id="history">History</button> 
+        <span class="myOrders-nav__item-element invisible"></span> 
+      </div> 
+    </div> 
+           
+    <div class="myOrders__table1"> 
+      <!-- Текущие заказы будут отображаться здесь --> 
+    </div> 
+ 
+    <div class="myOrders__table2 invisible"> 
+      <!-- История заказов будет отображаться здесь --> 
+    </div> 
+  </div>`;
 
-          <div class="myOrders-table1__item">
-              <div class="item__block1">
-                <p class="item-block1__text">24 June | 12:30 | by 18:10</p>
-
-                <div class="item-block1__element">
-                  <img class="item-block1__element-img" src="./src/assets/images/coffee-icons/chosen-beverage.svg">
-                  <p class="item-block1__element-text">Americano</p>
-                </div>
-
-                <div class="item-block1__element">
-                  <img class="item-block1__element-img" src="./src/assets/images/chosen-store.svg">
-                  <p class="item-block1__element-text">Bradford BD1 1PR</p>
-                </div>
-              </div>
-
-              <div class="item__block2">
-               <h2 class="item-block2__text">BYN 3.00</h2>
-              </div>
-
-          </div>
-
-        </div>
-
-        <div class="myOrders__table2 invisible">
-
-        </div>
-
-      </main>
-  `;
-
-  //Подключение кнопок On Going и History
-
-  //Кнопки
   const onGoing = document.getElementById("onGoing");
   const history = document.getElementById("history");
 
-  //Табы
   const onGoingTab = document.querySelector(".myOrders__table1");
   const onGoingParent = onGoing.closest("div");
   const onGoingSpan = onGoingParent.querySelector(
     ".myOrders-nav__item-element"
   );
-
   const historyTab = document.querySelector(".myOrders__table2");
   const historyParent = history.closest("div");
   const historySpan = historyParent.querySelector(
     ".myOrders-nav__item-element"
   );
 
-  //Кнопка On Going
+  // Кнопка On Going
   onGoing.addEventListener("click", (e) => {
     historyTab.classList.add("invisible");
     onGoingTab.classList.remove("invisible");
@@ -80,7 +65,7 @@ export default function renderMyOrdersPage(main) {
     onGoing.classList.add("border");
   });
 
-  //Кнопка History
+  // Кнопка History
   history.addEventListener("click", (e) => {
     onGoingTab.classList.add("invisible");
     historyTab.classList.remove("invisible");
@@ -92,48 +77,70 @@ export default function renderMyOrdersPage(main) {
     onGoing.classList.remove("border");
   });
 
-  //Логика для вкладки History
-  const historyOrders = JSON.parse(localStorage.getItem("order"));
-
-  for (let key in historyOrders) {
-    const orderDate = historyOrders[key].order_date;
-    const orderTime = historyOrders[key].order_time;
-    const coffeeType = historyOrders[key].coffee_type;
-    const cafeAddress = historyOrders[key].cafe_address;
-    const orderPrice = historyOrders[key].order_price;
-
-    const orderContainer = document.createElement("div");
-    orderContainer.classList.add(".myOrders-table2__item");
-    const orderRecap = `
-              <div class="item__block1">
-                <p class="item-block1__text">${orderDate} | ${orderTime}</p>
-
-                <div class="item-block1__element">
-                  <img class="item-block1__element-img" src="./src/assets/images/coffee-icons/chosen-beverage.svg">
-                  <p class="item-block1__element-text">${coffeeType}</p>
-                </div>
-
-                <div class="item-block1__element">
-                  <img class="item-block1__element-img" src="./src/assets/images/chosen-store.svg">
-                  <p class="item-block1__element-text">${cafeAddress}</p>
-                </div>
-              </div>
-
-              <div class="item__block2">
-               <h2 class="item-block2__text">BYN ${orderPrice}</h2>
-               <button class="item-block2__button">Order</button>
-              </div>
+  // Функция для создания HTML-элемента заказа
+  function createOrderElement(orderKey, orderData) {
+    return ` 
+      <div class="myOrders-table1__item" id="${orderKey}"> 
+        <div class="item__block1"> 
+          <p class="item-block1__text">${formatDateTime(
+            orderData.order_time
+          )} | by ${orderData.order_time}</p> 
+          <div class="item-block1__element"> 
+            <div class="item-block1__element-img">${CoffeeCupImg}</div> 
+            <p class="item-block1__element-text">${orderData.coffee_type}</p> 
+          </div> 
+          <div class="item-block1__element"> 
+            <div class="item-block1__element-img">${cafeAddressImg}</div> 
+            <p class="item-block1__element-text">${orderData.cafe_address}</p> 
+          </div> 
+        </div> 
+        <div class="item__block2"> 
+          <h2 class="item-block2__text">BYN ${orderData.order_price}</h2> 
+        </div> 
+      </div> 
     `;
-    orderContainer.innerHTML = orderRecap;
-    historyTab.prepend(orderContainer);
   }
-  /*
-  for (let x = 0; x < Object.keys(historyOrders).length; x++) {
-    console.log(historyOrders[x]);
-  }
-    */
 
-  /*console.log(
-    `My order will be ready at ${order.order_time}, it costs ${order.order_price}. You can pick up the order at ${order.cafe_address} and it's a ${order.coffee_type}`
-  );*/
+  // Функция для отображения текущих заказов
+  function displayOrders() {
+    onGoingTab.innerHTML = "";
+    for (let key of Object.keys(order)) {
+      if (!key.includes("history")) {
+        onGoingTab.innerHTML += createOrderElement(key, order[key]);
+      }
+    }
+  }
+
+  // Функция для перемещения заказа из текущих в историю
+  function moveToHistory(orderKey) {
+    const orderData = order[orderKey];
+    delete order[orderKey];
+    order[`${orderKey}_history`] = orderData;
+    localStorage.setItem("order", JSON.stringify(order));
+    displayOrders();
+    displayHistory();
+  }
+
+  // Функция для отображения истории заказов
+  function displayHistory() {
+    historyTab.innerHTML = "";
+    for (let key of Object.keys(order)) {
+      if (key.includes("history")) {
+        historyTab.innerHTML += createOrderElement(key, order[key]);
+      }
+    }
+  }
+
+  // Таймер для перемещения заказов в историю через 30 минут
+  function setOrderTimers() {
+    for (let key of Object.keys(order)) {
+      if (!key.includes("history")) {
+        setTimeout(() => moveToHistory(key), 5 * 60 * 1000);
+      }
+    }
+  }
+
+  displayOrders();
+  displayHistory();
+  setOrderTimers();
 }
