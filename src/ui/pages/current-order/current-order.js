752be91e7ed8983e4365e88b0d2dfa7a01b240fe
Margo-Bond <ritabond1991@gmail.com>
2024-07-12 19:10:02 +0300

    merge

--- src/ui/pages/current-order/current-order.js
+++ src/ui/pages/current-order/current-order.js
@@ -17,12 +17,17 @@ export default function renderCurrentOrderPage(main) {
   let lastKey = keys[keys.length - 1];
   const backBtn = document.querySelector(".current-header__back-btn");
   const nextBtn = document.querySelector(".current__button-next");
+  const currentOrderContainer = document.getElementById(
+    "currentOrderContainer"
+  );
   const cafeAddress = order[lastKey].cafe_address;
   const coffeeType = order[lastKey].coffee_type;
   const orderPrice = order[lastKey].order_price;
+  const coffeeIce = order[lastKey].coffee_ice;
+  const milkOption = order[lastKey].milk_option;
+  const syrupOption = order[lastKey].syrup_option;
   const cupQuantity = order[lastKey].cup_quantity || "none";
   const coffeeRistretto = order[lastKey].coffee_ristretto || "none";
-  const coffeeIce = order[lastKey].coffee_ice;
   const coffeeRoasting = order[lastKey].coffee_roasting || "none";
   const cafeOne = "Bradford BD1 1PR";
   const cafeTwo = "Bradford BD4 7SJ";
@@ -70,20 +75,13 @@ export default function renderCurrentOrderPage(main) {
     try {
       const cafes = await getCafes();
       if (cafes) {
-        if (cafes[cafeKey]) {
-          const coffeeData = await getCoffeeType(cafeKey, coffeeKey);
-          if (coffeeData && coffeeData.image_url) {
-            const coffeeImage = document.querySelector(".current-order__img");
-            coffeeImage.src = coffeeData.image_url;
-            coffeeImage.alt = coffeeType;
-          } else {
-            console.log("No coffee data available");
-          }
-        } else {
-          console.log("Cafe not found");
-        }
+        const coffeeData = await getCoffeeType(cafeKey, coffeeKey);
+        const coffeeImage = document.querySelector(".current-order__item-img");
+        const coffeeType = order[lastKey].coffee_type;
+        coffeeImage.src = coffeeData.image_url;
+        coffeeImage.alt = coffeeType;
       } else {
-        console.log("No cafes data available");
+        console.log("No coffee data available");
       }
     } catch (error) {
       console.error("Error setting coffee image:", error);
@@ -94,71 +92,24 @@ export default function renderCurrentOrderPage(main) {
     setCoffeeImage(cafeKey, coffeeKey);
   }
 
-  // Функция для создания объекта заказа
-  function createOrderObject() {
-    return {
-      coffee_roasting: localStorage.getItem("coffee_coffee_roasting") || "none",
-      orders: JSON.parse(localStorage.getItem("orders")) || [],
-      syrup_option: localStorage.getItem("syrup_option") || "none",
-      mug_option: localStorage.getItem("mug_option") || "none",
-      coffee_ristretto: localStorage.getItem("coffee_ristretto") || "none",
-      user: JSON.parse(localStorage.getItem("user")) || {},
-      coffee_type: localStorage.getItem("coffee_type") || "none",
-      order_price: parseFloat(localStorage.getItem("order_price")) || 0,
-      cup_quantity: parseInt(localStorage.getItem("cup_quantity")) || 1,
-      cafe_address: localStorage.getItem("cafe_address") || "none",
-      order_time: localStorage.getItem("order_time") || "none",
-      milk_option: localStorage.getItem("milk_option") || "none",
-      cup_volume: localStorage.getItem("cup_volume") || "none",
-      coffee_ratio: parseInt(localStorage.getItem("coffee_ratio")) || 0,
-      coffee_grinding: localStorage.getItem("coffee_coffee_grinding") || "none",
-    };
-  }
-  // Маршрутизация
-  backBtn.addEventListener("click", () => {
-    window.location.href = "/designer";
-  });
-
-  nextBtn.addEventListener("click", () => {
-    window.location.href = "/order-confirmed";
-  });
-
-  // Функция для добавления нового заказа
-  function addOrder() {
-    let orders = JSON.parse(localStorage.getItem("order")) || [];
-    let newOrder = createOrderObject();
-    orders.push(newOrder);
-    localStorage.setItem("order", JSON.stringify(orders));
-  }
-
-  // Вызов функции для добавления нового заказа
-  addOrder();
-
   // Функция для отображения заказов
-  function displayOrder() {
-    const currentOrderContainer = document.getElementById(
-      "currentOrderContainer"
-    );
+  function displayOrder(order) {
     currentOrderContainer.innerHTML = "";
 
-    let order = JSON.parse(localStorage.getItem("order")) || [];
-
-    order.forEach((order) => {
-      const orderItem = document.createElement("div");
+    order.forEach((orderItem) => {
+      const order = document.createElement("div");
       orderItem.classList.add("current-order__item");
 
       orderItem.innerHTML = `
-        <img class="current-order__img" src="${
-          order.image_url || "path/to/default_image.jpg"
-        }" alt="${order.coffee_type}" class="current-order__item-img"/>
+        <img class="current-order__item-img" src="${order.image_url}" alt="${order[lastKey].coffee_type}"/>
         <div class="current-order__item-description">
-          <p class="current-order__item-title">${order.coffee_type}</p>
-          <p class="current-order__item-details">${order.details || ""}</p>
-          <p class="current-order__item-quantity">x ${order.cup_quantity}</p>
+          <p class="current-order__item-title">${order[lastKey].coffee_type}</p>
+          <p class="current-order__item-details">${orderp[lastKey].details}</p>
+          <p class="current-order__item-quantity">x ${order[lastKey].cup_quantity}</p>
         </div>
         <div class="current-order__item-sum">
           <p class="current-order__item-currency">BYN</p>
-          <p class="current-order__item-count">${order.order_price}</p>
+          <p class="current-order__item-count">${order[lastKey].order_price}</p>
         </div>
       `;
 
@@ -167,5 +118,14 @@ export default function renderCurrentOrderPage(main) {
   }
 
   // Отображаем заказы при загрузке страницы
-  displayOrder();
+  displayOrder(order);
+
+  // Маршрутизация
+  backBtn.addEventListener("click", () => {
+    window.location.href = "/designer";
+  });
+
+  nextBtn.addEventListener("click", () => {
+    window.location.href = "/order-confirmed";
+  });
 }
